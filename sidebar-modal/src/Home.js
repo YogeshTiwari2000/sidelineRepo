e62b444be37f5416072f9951f28d1836d21ac4c0
YogeshTiwari2000 <84225515+YogeshTiwari2000@users.noreply.github.com>
2024-07-08 18:05:36 +0530

    Add files via upload

---
+++ sidebar-modal/src/Home.js
@@ -0,0 +1,17 @@
+import React, { useContext } from 'react'
+import { FaBars } from 'react-icons/fa'
+import { AppContext, useGlobalContext } from './context'
+
+const Home = () => {
+  const { openSidebar, openModal } = useGlobalContext()
+
+  const data = useGlobalContext();
+  console.log(data)
+
+  return <main>
+    <button className='sidebar-toggle' onClick={openSidebar}><FaBars /></button>
+    <button className='btn' onClick={openModal}>Show Modal</button>
+  </main>
+}
+
+export default Home
